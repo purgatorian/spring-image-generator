671cf396c9c0fa741bd0456e32f41d2fcc068137
purgatorian <memedkipik@gmail.com>
2025-01-16 18:48:04 +0300

    neon database

--- app/api/collections/[id]/add-image/route.ts
+++ app/api/collections/[id]/add-image/route.ts
@@ -1,3 +1,5 @@
+//app/api/collections/[id]/add-image/route.ts
+
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getAuth } from "@clerk/nextjs/server";
